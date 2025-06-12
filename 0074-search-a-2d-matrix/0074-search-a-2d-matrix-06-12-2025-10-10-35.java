85ea3429f169ac1f0c5002685a7d388cec9a107a
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2025-06-12 10:10:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix-06-12-2025-10-10-35.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int prod=matrix.length*matrix[0].length;
+
+        return binarySearch(matrix, 0, prod, target);
+    }
+
+    public boolean binarySearch(int[][] matrix, int start, int end, int target){
+
+        if(start>end) return false;
+        int mid=start+(end-start)/2;
+        int j=mid%matrix[0].length;
+        int i=mid/matrix[0].length;
+
+        if(target==matrix[i][j]) return true;
+
+        if(target>matrix[i][j]) return binarySearch(matrix, mid+1, end, target);
+
+        if(target<matrix[i][j]) return binarySearch(matrix, start, mid-1, target);
+
+        return false;
+    }
+}
