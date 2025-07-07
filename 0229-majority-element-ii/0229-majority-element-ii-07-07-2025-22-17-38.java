ddc02038aa0624f79534922b09ceaa8a64d7cba8
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2025-07-07 22:17:39 +0530

    Time: 2 ms (99.69%), Space: 50.4 MB (41.7%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii-07-07-2025-22-17-38.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        int ele1=nums[0],ele2=0,count1=1,count2=0,n=nums.length;
+
+        List<Integer> list=new LinkedList<>();
+
+        for(int i=1;i<n;i++){
+            if(ele1==nums[i]) count1++;
+            else if(ele2==nums[i]) count2++;
+            else if(count1==0 && ele2!=nums[i]){
+                ele1=nums[i];
+                count1++;
+            } else if (count2==0 && ele1!=nums[i]){
+                ele2=nums[i];
+                count2++;
+            } 
+            else {
+                count1--;
+                count2--;
+            }
+
+
+        }
+
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele1) count1++;
+            else if(nums[i]==ele2) count2++;
+        }
+
+        if(count1>=(int)(n/3)+1) list.add(ele1);
+        if(count2>=(int)(n/3)+1 && ele1!=ele2) list.add(ele2);
+
+        return list;
+    }
+}
