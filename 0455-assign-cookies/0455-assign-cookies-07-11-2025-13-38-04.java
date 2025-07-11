4584f7bee20e8a81b112f809c92d49008aa57110
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2025-07-11 13:38:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies-07-11-2025-13-38-04.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int ng=g.length,ns=s.length;
+        if(ns==0) return 0;
+        int i=0,j=0,count=0;
+
+        while(i<ng && j<ns){
+            if(g[i]<=s[j]){
+                count++;
+                i++;
+                j++;
+            }else j++;
+        }
+
+        return count;
+
+
+    }
+}
