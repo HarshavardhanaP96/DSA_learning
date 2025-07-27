655d0e4f52b6818d834b2c45d33f585f81434055
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2025-07-27 21:48:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths-07-27-2025-21-48-09.java
@@ -0,0 +1,22 @@
+class Solution {
+    int count=0;
+    public int uniquePaths(int m, int n) {
+
+        //Number of steps required to reach the end point
+        int steps=m+n-2;
+
+        //Mathematically using binomial coefficiant approach
+        //number of ways=(m+n-2)!/[(m-1)!*(n-1)!]
+
+        //code is looped after further simplification of mathematical computation.
+
+        int minLoop=(m<n)?m-1:n-1;
+
+        int res=1;
+        for( int i=1;i<=minLoop;i++){
+            res=res*(steps-minLoop+i)/i;
+        }
+
+        return res;
+    }
+}
