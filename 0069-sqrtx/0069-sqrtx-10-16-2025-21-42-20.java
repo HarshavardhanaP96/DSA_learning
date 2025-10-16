b17360fbbf2b2c2c78b35a4536f204714485c045
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2025-10-16 21:42:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx-10-16-2025-21-42-20.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low=1,high=x,ans=0;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(mid*mid==x) return mid;
+            else if(mid*mid>x){
+                high=mid-1;
+            }else if(mid*mid<x){
+                if(mid>ans) ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
