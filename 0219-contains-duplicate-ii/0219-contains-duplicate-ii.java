8c89c1ee974cf476479d040eedbb28d530476af1
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2026-03-09 22:29:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -2,22 +2,11 @@ import java.util.HashMap;
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Map stores: Key = Number, Value = Its most recent index
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            // If we've seen this number before...
-            if (map.containsKey(nums[i])) {
-                // Check if the distance between current and previous index is <= k
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                }
+        Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (i > k) set.remove(nums[i - k - 1]); // Keep the window size at k
+                if (!set.add(nums[i])) return true;     // If add fails, a duplicate exists in range
             }
-            
-            // Update the map with the latest index for this number
-            map.put(nums[i], i);
-        }
-
         return false;
     }
 }
