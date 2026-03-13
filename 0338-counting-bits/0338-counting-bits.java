51ebc52f6f8251a450f3a1dee90723e0aac98bc3
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2026-03-13 12:22:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,16 @@
 class Solution {
+
+    //using dp
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
-        
-        for (int i = 0; i <= n; i++) {
-            int ele = i;
-            int count = 0;
-            while (ele > 0) {
-                ele &= (ele - 1); // Clears the lowest set bit
-                count++;
-            }
-            ans[i] = count;
+        for (int i = 1; i <= n; i++) {
+
+            //The number of bits in $i$ is just the number of bits in $i/2$ plus $1$ if $i$ is odd.
+            //If $i$ is even: ans[i] = ans[i >> 1]
+            //If $i$ is odd: ans[i] = ans[i >> 1] + 1
+            // ans[i] = ans[i / 2] + (i % 2)
+            ans[i] = ans[i >> 1] + (i & 1);
         }
-        
         return ans;
     }
 }
