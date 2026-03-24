4075e29be923eed07c2b80ef203a70b7ad3b4e7a
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2026-03-24 22:58:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,15 +3,12 @@ class Solution {
         int[] ans=new int[nums.length];
         Arrays.fill(ans,1);
 
-        int product=1;
-        
-        for(int i=0;i<nums.length;i++){
-            ans[i]= ans[i]*product;
-            product*=nums[i];
-
+        ans[0]=1;        
+        for(int i=1;i<nums.length;i++){
+            ans[i]= ans[i-1]*nums[i-1];
         }
 
-        product=1;
+        int product=1;
         for(int i=nums.length-1;i>=0;i--){
             ans[i]=ans[i]*product;
             product*=nums[i];
