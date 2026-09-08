5dcdc0c552dd820afb8570d29bb4b35a79e5564c
Harshavardhana P <68232905+HarshavardhanaP96@users.noreply.github.com>
2026-09-08 20:26:27 +0530

    Time: 2 ms (73.94%), Space: 42.4 MB (82.04%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -4,39 +4,60 @@ class Solution {
     public int distributeCookies(int[] cookies, int k) {
         
         Arrays.sort(cookies);
+        reverse(cookies);
         int[] children =new int[k];
 
 
-        dfs(cookies, 0, children, k);
+        dfs(cookies, 0, children, 0);
         return minFairness;
     
     }
 
 
-    public void dfs(int[] cookies, int index, int[] children, int k) {
+    public void dfs(int[] cookies, int index, int[] children, int currentMax) {
+
+        if(currentMax>=minFairness) return;
 
         if(index==cookies.length){
-            int max=0;
-            for(int child:children){
-                max=Math.max(child, max);
-            }
 
-            minFairness=Math.min(minFairness,max);
+            minFairness=currentMax;
             return;
         }
 
-        for(int i=0;i<k;i++){
+        for(int i=0;i<children.length;i++){
 
             if(children[i]+cookies[index]>=minFairness) continue;
 
+
             children[i]+=cookies[index];
-            dfs(cookies, index+1, children, k);
+            dfs(cookies, index+1, children, Math.max(currentMax,children[i]));
 
 
             children[i]-=cookies[index];
+
+
+            if (children[i] == 0) {
+                break;
+            }
         }
 
 
+    }
+
+    public void reverse(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+            
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+
+            start++;
+            end--;
+    
+        }
     }
 
         
